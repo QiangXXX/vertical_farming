b0ca79f172976e31c3a6d3e78c825f4f6beb3e7f
QiangXXX <124871720+QiangXXX@users.noreply.github.com>
2023-05-20 19:44:19 +0200

    Delete src directory

--- src/inc/veml7700_reg.h
+++
@@ -1,9 +0,0 @@
-/*
-VEML7700 REGISTERS
-*/
-
-#define VEML7700_High_Resolution_Output_Data 0x04 //MSB 8 bits data of whole ALS 16 bits
-//LSB 8 bits data of whole ALS 16 bits
-#define VEML7700_WHITE 0x05//MSB 8 bits data of whole WHITE 16 bits
-//LSB 8 bits data of whole WHITE 16 bits
-#define VEML7700_trigger 0x06//ALS INT trigger event
